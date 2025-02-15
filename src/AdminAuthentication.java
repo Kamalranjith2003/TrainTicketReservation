404f48bd47c96a89ee4a6c29818ff87e40ea63ec
Kamal Ranjith S <148188631+Kamalranjith2003@users.noreply.github.com>
2025-02-15 12:18:48 +0530

    Add files via upload

---
+++ src/AdminAuthentication.java
@@ -0,0 +1,5 @@
+public class AdminAuthentication {
+    public static boolean login(String username, String password) {
+            return username.equals("admin") && password.equals("admin123");
+    }
+}
